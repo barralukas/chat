bb6b46d82b6879a1cf0ff0bd69f7931691057a10
Lucas <lucasna256@gmail.com>
2023-11-11 14:14:28 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -95,7 +95,7 @@ const handleLogin = (event) => {
   login.style.display = "none"
   chat.style.display = "flex"
 
-  websocket = new WebSocket("ws://localhost:8080")
+  websocket = new WebSocket("wss://chat-backend-cagu.onrender.com")
   websocket.onmessage = processMessage
 }
 
